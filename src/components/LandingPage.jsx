e1ddb674599817392bc57f6b145520f7c653b135
keyur6021 <86195043+keyur6021@users.noreply.github.com>
2022-12-03 15:02:10 +0530

    add responsive video

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import landing from './../assets/image/landing.jpg'
+import backgroundVideo from './../assets/image/videos/background.mp4'
 
 const LandingPage = () => {
     return (
         <div className='main-image'>
-            <img src={landing} alt="landingPage image" className='demo-image' />
+            {/* <img src={landing} alt="landingPage image" className='demo-image' /> */}
+            <video src={backgroundVideo} autoPlay loop muted />
+            <div className='heading'>
+                <div>
+                    WEL-COME
+                </div>
+                <div>
+                    My Web Resume
+                </div>
+            </div>
         </div>
     )
 }
