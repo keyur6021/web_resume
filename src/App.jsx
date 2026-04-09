ae877e302dabaece4418c904eb82f757acb986e9
Keyur <keyur@limbanisoftwares.com>
2026-04-09 18:40:03 +0530

    app updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Templates from './pages/Templates';
-import Editor from './pages/Editor';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Templates from "./pages/Templates";
+import Editor from "./pages/Editor";
 
 function App() {
+  
   return (
     <BrowserRouter>
       <Routes>
